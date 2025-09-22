605d2f403092431969f330522d98c9771171fb76
Tiago-Egas <tiagoegas@outlook.com>
2025-09-22 14:51:54 -0300

    Create customer, validate CPF
    Signed-off-by: Tiago-Egas <tiagoegas@outlook.com>

--- src/index.js
+++ src/index.js
@@ -1,5 +1,41 @@
 const express = require('express');
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+/**
+ * cpf - string
+ * name - string
+ * id - uuid
+ * statement []
+ */
+app.post("/account", (req, res) => {
+    const { cpf, name } = req.body;
+
+    const customersAlreadyExist = customers.some(
+        (customer) => customer.cpf === cpf
+    );
+
+    if (customersAlreadyExist) {
+        return res.status(400).send({
+            error: "Customer already exists!"
+        });
+    }
+
+    const id = uuidv4();
+
+    customers.push({
+        cpf,
+        name,
+        id: uuidv4(),
+        statement: [],
+    })
+
+    return res.status(201).send({"message": "Customer created"});
+})
 
 app.listen(3333);
